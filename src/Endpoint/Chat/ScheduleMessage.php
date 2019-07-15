2147d2aeeb0f34b363d16addeec1f41adc83eeee
Matthijs Thoolen <matthijsthoolen@hotmail.com>
2019-07-15 23:20:38 +0200

    Added ephemeral and scheduled messages

---
+++ src/Endpoint/Chat/ScheduleMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MatthijsThoolen\Slacky\Endpoint\Chat;
+
+use MatthijsThoolen\Slacky\Endpoint\Endpoint;
+use MatthijsThoolen\Slacky\Model\Message\Message;
+use MatthijsThoolen\Slacky\Model\SlackyResponse;
+
+/**
+ * @documentation https://api.slack.com/methods/chat.scheduleMessage
+ */
+class ScheduleMessage extends Endpoint
+{
+    /** @var string */
+    protected $method = 'POST';
+
+    /** @var string */
+    protected $uri = 'chat.scheduleMessage';
+
+    /** @var Message */
+    protected $message;
+
+    /**
+     * @param Message $message
+     * @return $this
+     */
+    public function setMessage(Message $message)
+    {
+        $this->message = $this->parameters = $message;
+
+        return $this;
+    }
+
+    /**
+     * @param SlackyResponse $response
+     * @return SlackyResponse
+     * @throws \Exception
+     */
+    public function handleResponse(SlackyResponse $response)
+    {
+        parent::handleResponse($response);
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $body = $response->getMessage();
+        $this->message->loadData($body);
+
+        $response->setObject($this->message);
+
+        return $response;
+    }
+}
---
+++ tests/Endpoint/Chat/ScheduledMessageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MatthijsThoolen\Slacky\Tests\Endpoint\Chat;
+
+use function getenv;
+use MatthijsThoolen\Slacky\Endpoint\Chat\PostMessage;
+use MatthijsThoolen\Slacky\Endpoint\Chat\ScheduleMessage;
+use MatthijsThoolen\Slacky\Model\Message\Message;
+use MatthijsThoolen\Slacky\Model\SlackyResponse;
+use MatthijsThoolen\Slacky\Slacky;
+use MatthijsThoolen\Slacky\SlackyFactory;
+use PHPUnit\Framework\TestCase;
+
+class ScheduledMessageTest extends TestCase
+{
+
+    public function testScheduleMessage()
+    {
+        $slackToken = getenv('SLACK_BOT_TOKEN');
+        new Slacky($slackToken);
+
+        /** @var PostMessage $postMessage */
+        $postMessage = SlackyFactory::build(ScheduleMessage::class);
+        self::assertInstanceOf(ScheduleMessage::class, $postMessage);
+
+        $message = new Message();
+        $message->setChannel(getenv('SLACK_PHPUNIT_CHANNEL'));
+        $message->setPostAt(time() + 15);
+        $message->setText('A unit test scheduled for the future!');
+
+        $response = $postMessage->setMessage($message)->send();
+        self::assertInstanceOf(SlackyResponse::class, $response);
+        self::assertSame(200, $response->getStatusCode());
+
+        $body = $response->getBody();
+        self::assertNotNull($body['scheduled_message_id']);
+        self::assertTrue($response->isOk());
+        self::assertEquals('OK', $response->getError());
+
+        return $message;
+    }
+
+}
