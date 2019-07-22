bda385938e61402f4a204dc3a4ca72fbef918649
Matthijs Thoolen <matthijsthoolen@hotmail.com>
2019-07-22 22:46:54 +0200

    Add MeMessage

---
+++ src/Endpoint/Chat/MeMessage.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace MatthijsThoolen\Slacky\Endpoint\Chat;
+
+use MatthijsThoolen\Slacky\Endpoint\Endpoint;
+use MatthijsThoolen\Slacky\Exception\SlackyException;
+use MatthijsThoolen\Slacky\Model\SlackyResponse;
+
+class MeMessage extends Endpoint
+{
+    /** @var string */
+    protected $method = 'POST';
+
+    /** @var string */
+    protected $uri = 'chat.meMessage';
+
+    /** @var string */
+    private $channel;
+
+    /** @var string */
+    private $text;
+
+    /**
+     * @return string
+     */
+    public function getChannel(): string
+    {
+        return $this->channel;
+    }
+
+    /**
+     * @param string $channel
+     * @return MeMessage
+     */
+    public function setChannel(string $channel): MeMessage
+    {
+        $this->channel = $this->parameters['channel'] = $channel;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return MeMessage
+     */
+    public function setText(string $text): MeMessage
+    {
+        $this->text = $this->parameters['text'] = $text;
+        return $this;
+    }
+
+    /**
+     * @param SlackyResponse $response
+     * @return SlackyResponse
+     * @throws SlackyException
+     */
+    public function handleResponse(SlackyResponse $response)
+    {
+        parent::handleResponse($response);
+
+        return $response;
+    }
+
+}
---
+++ tests/Endpoint/Chat/MeMessageTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MatthijsThoolen\Slacky\Tests\Endpoint\Chat;
+
+use MatthijsThoolen\Slacky\Endpoint\Chat\MeMessage;
+use MatthijsThoolen\Slacky\Exception\SlackyException;
+use MatthijsThoolen\Slacky\Model\SlackyResponse;
+use MatthijsThoolen\Slacky\SlackyFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MeMessageTest
+ * @coversDefaultClass \MatthijsThoolen\Slacky\Endpoint\Chat\MeMessage
+ */
+class MeMessageTest extends TestCase
+{
+
+    /**
+     * @covers ::handleResponse
+     * @covers ::setText
+     * @covers ::setChannel
+     * @throws SlackyException
+     */
+    public function testMeMessage()
+    {
+        /** @var MeMessage $meMessage */
+        $meMessage = SlackyFactory::build(MeMessage::class);
+        self::assertInstanceOf(MeMessage::class, $meMessage);
+
+        $response = $meMessage
+            ->setChannel(getenv('SLACK_PHPUNIT_CHANNEL'))
+            ->setText('This is a meMessage. Hi me! You go me! You are doin\' great!')
+            ->send();
+
+        self::assertInstanceOf(SlackyResponse::class, $response);
+        self::assertSame(200, $response->getStatusCode());
+        self::assertTrue($response->isOk());
+    }
+}
